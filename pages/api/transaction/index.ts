9b780f1a267321a5596e040939ea00047ab0ad89
Ezequiel <ezequielezequiel9@gmail.com>
2025-06-05 18:38:42 -0300

    add micro-method-router

--- pages/api/transaction/index.ts
+++ pages/api/transaction/index.ts
@@ -1,17 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createTransaction } from "controllers/transaction";
+import method from "micro-method-router";
 
-export default async function order(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      const result = await createTransaction(req.body);
-      if (!result.success) {
-        return res.status(404).json(result);
-      } else {
-        res.status(201).json(result);
-      }
-    } catch (error) {
-      return res.status(500).json({ success: false, message: error.message });
+export async function transaction(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const result = await createTransaction(req.body);
+    if (!result.success) {
+      return res.status(404).json(result);
+    } else {
+      res.status(201).json(result);
     }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
   }
 }
+
+export default method({
+  post: transaction,
+});
